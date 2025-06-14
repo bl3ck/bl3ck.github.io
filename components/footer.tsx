9464db9f50016fdd283a5f123e60590a0d0c626f
Eugen <eugenbleck@gmail.com>
2025-06-14 01:26:32 -0700

    feat: setup frontpage

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-import { ContainerInner, ContainerOuter } from '@/components/container'
+import { ContainerInner, ContainerOuter } from '@/components/Container'
 
 function NavLink({
   href,
